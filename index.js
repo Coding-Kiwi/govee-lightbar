66556df83f3d4cc0ba7b8557145a1e2605e181c6
CodingKiwi <erikweber84@gmail.com>
2022-03-09 20:28:11 +0100

    play around

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const noble = require("@abandonware/noble");
+const readline = require('readline');
 
 const MODEL = "Govee_H6054_1146";
 const SERVICE = "000102030405060708090a0b0c0d1910";
@@ -22,6 +23,21 @@ function assembleMessageWithChecksum(bytes) {
     });
 
     return [CONTROL_PACKET_ID, ...bytes, checksum].map(hexify).join("");
+}
+
+let writeCharacteristic;
+
+async function sendMessage(message) {
+    await writeCharacteristic.writeAsync(Buffer.from(message, "hex"), false);
+}
+
+async function setRGB(r, g, b, s1, s2) {
+    await sendMessage(assembleMessageWithChecksum([
+        0x05, 0x15, 0x01,
+        r, g, b,
+        0x00, 0x00, 0x00, 0x00, 0x00,
+        s1, s2
+    ]));
 }
 
 noble.on("discover", async (peripheral) => {
@@ -56,15 +72,70 @@ noble.on("discover", async (peripheral) => {
         return
     }
 
-    let writeCharacteristic = stuffFound.characteristics.find(c => {
+    writeCharacteristic = stuffFound.characteristics.find(c => {
         return c._serviceUuid === SERVICE && c.uuid === WRITE_CHARACTERISTIC;
     });
 
-    async function sendMessage(message) {
-        await writeCharacteristic.writeAsync(Buffer.from(message, "hex"), false);
+    //0x33 = power
+    await sendMessage(assembleMessageWithChecksum([
+        0x33, 0x11
+    ])); // both on
+
+    //warm white
+    /* await sendMessage(assembleMessageWithChecksum([
+        0x05, 0x15, 0x01, 0xFF, 0xFF, 0xFF, 0x07, 0xd0, 0xff, 0x89, 0x12,
+        0x00, 0x0F
+    ])); */
+
+    //0x04 = brightness
+    await sendMessage(assembleMessageWithChecksum([
+        0x04, 0x64
+    ])); //full brightness for both
+
+    /* let s = 0;
+
+    async function next() {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
+
+        //0x05 = color
+        await setRGB(0x00, 0x00, 0x00, 0xFF, 0x0F);
+        await setRGB(0xff, 0x00, 0x00, s, 0x00);
+
+        return new Promise(resolve => rl.question(s, ans => {
+            rl.close();
+            s++;
+            resolve();
+        }))
     }
 
-    sendMessage(assembleMessageWithChecksum([0x33, 0x11])); // both on
+    //0x05 = color
+    while (s < 256) {
+        await next();
+    } */
+
+    function blink() {
+        setRGB(0x00, 0x00, 0x00, 0x55, 0x05);
+        setRGB(0x00, 0x00, 0xff, 0xAA, 0x0A);
+
+        setTimeout(() => {
+            setRGB(0x00, 0x00, 0x00, 0xAA, 0x0A);
+            setRGB(0xff, 0x00, 0x00, 0x55, 0x05);
+        }, 1000);
+    }
+
+    setInterval(blink, 2000);
+
+    return;
+
+    // not possible in white mode
+    await sendMessage(assembleMessageWithChecksum([
+        0x05, 0x15, 0x02,
+        0x32,
+        0x01, 0x00
+    ])); //brightness only for left half
 });
 
 
